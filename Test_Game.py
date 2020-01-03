3bac2ad211ea76125dd5139c11ec2f06ad19441e
egor-lihverov <57951669+egor-lihverov@users.noreply.github.com>
2020-01-03 23:55:09 +0700

    Add files via upload

--- Test_Game.py
+++ Test_Game.py
@@ -1,10 +1,12 @@
 import os
 import random
 import sys
-
 import pygame
+from pygame.display import set_mode
 
 pygame.init()
+
+import ctypes
 
 
 class Camera:
@@ -28,10 +30,17 @@ camera = Camera()
 
 tile_width = tile_height = 40
 
-size = width, height = 1000, 1000
+user32 = ctypes.windll.user32
+screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+size = width, height = 1000, 770
 screen = pygame.display.set_mode(size)
 running = True
 all_sprites = pygame.sprite.Group()
+
+
+def draw_backpack():
+    backpack = load_image("backpack.png", -1)
+    screen.blit(backpack, (0, 0))
 
 
 def load_level(filename):
@@ -44,7 +53,7 @@ def load_level(filename):
     max_width = max(map(len, level_map))
 
     # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+    return list(map(lambda x: x.ljust(max_width, ','), level_map))
 
 
 def load_image(name, color_key=None):
@@ -63,29 +72,37 @@ def generate_level(level):
     new_player, i, j = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            Floor(x, y)
-            if level[y][x] == '#':
+            if level[y][x] == '.':
+                Floor(x, y)
+            elif level[y][x] == '#':
                 Border(x, y)
-                Botton_Wall(x, y)
+                if y + 1 != '#':
+                    Botton_Wall(x, y)
             elif level[y][x] == '@':
+                Floor(x, y)
                 i, j = x, y
-
+    Weapon(6, 15, 'one_punch')
+    #Weapon(7, 17, 'one_punch')
     new_player = Knight(i, j)
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '#':
                 Top_Wall(x, y)
+    draw_backpack()
     # вернем игрока, а также размер поля в клетках
     return new_player
 
 
 class Knight(pygame.sprite.Sprite):
-    knight = load_image("Knight2.png", -1)
+    knight = load_image("Разбойник.png", -1)
 
     def __init__(self, pos_x, pos_y):
         super().__init__(all_sprites)
         self.image = Knight.knight
+        self.add(hero)
         self.rect = self.image.get_rect()
+        self.weapons = [0, 0]
+        self.weapon_selected = 0
         # вычисляем маску для эффективного сравнения
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
@@ -111,14 +128,21 @@ class Knight(pygame.sprite.Sprite):
         elif keys[pygame.K_DOWN]:
             self.rect = self.rect.move(0, speed)
             y = speed
-
         while pygame.sprite.spritecollideany(self, borders):
             self.rect = self.rect.move(0, -y)
-        # all_sprites.draw(screen)
 
+        if pygame.sprite.spritecollideany(self, weapon):
+            weapon_bar = load_image("Weapon_bar.png", -1)
+            gun = pygame.sprite.spritecollideany(self, weapon)
+            font = pygame.font.Font(None, 50)
+            text = font.render(gun.name, 1, (255, 255, 0))
+            screen.blit(text, (width // 2 - 210, height - 150))
+            screen.blit(weapon_bar, (width // 2 - 210, height - 120))
 
+hero = pygame.sprite.Group()
 borders = pygame.sprite.Group()
 decorations = pygame.sprite.Group()
+weapon = pygame.sprite.Group()
 
 
 class Border(pygame.sprite.Sprite):
@@ -164,6 +188,19 @@ class Floor(pygame.sprite.Sprite):
         # self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
+
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, name):
+        super().__init__(all_sprites)
+        self.add(weapon)
+        self.image = load_image("{}.png".format(name), -1)
+        self.rect = self.image.get_rect()
+        self.name = name
+        # вычисляем маску для эффективного сравнения
+        # self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
 def terminate():
     pygame.quit()
     sys.exit()
@@ -173,8 +210,7 @@ clock = pygame.time.Clock()
 
 level = load_level('level_1.txt')
 for i in level:
-    print(
-        *i)
+    print(*i)
 knight = generate_level(level)
 
 while True:
@@ -186,9 +222,8 @@ while True:
     # обновляем положение всех спрайтов
     for sprite in all_sprites:
         camera.apply(sprite)
-    screen.fill((255, 255, 255))
-
-    all_sprites.update(event)
+    screen.fill((0, 0, 0))
     all_sprites.draw(screen)
+    all_sprites.update(pygame.event.get())
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(80)
